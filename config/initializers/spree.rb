8f5b76a9d82f4cf2213548a32de5afb80cf8ff31
Gulshan Budhwani <gulkush@gmail.com>
2017-12-23 10:58:23 +0530

    image settings for s3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -19,26 +19,38 @@ Spree.config do |config|
     #production. Store images on S3.
     # development will default to local storage
     attachment_config = {
+
         s3_credentials: {
             access_key_id: ENV["S3_KEY"],
             secret_access_key: ENV["S3_SECRET"],
             bucket: ENV["S3_BUCKET"],
         },
 
-
         storage:        :s3,
+        s3_region:      "ap-south-1",
         s3_headers:     { "Cache-Control" => "max-age=31557600" },
         s3_protocol:    "https",
-        bucket:         ENV["S3_BUCKET"],
+        bucket:         "ap-south-1",
+        url:            "https://s3.ap-south-1.amazonaws.com",
 
-        path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
-        default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-        default_style: "product",
+        styles: {
+            mini:     "48x48>",
+            small:    "100x100>",
+            product:  "240x240>",
+            large:    "600x600>"
+        },
+
+        path:           "/:class/:id/:style/:basename.:extension",
+        default_url:    "/:class/:id/:style/:basename.:extension",
+        default_style:  "product"
     }
 
     attachment_config.each do |key, value|
       Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
     end
+
+
+
   end
 end
 
